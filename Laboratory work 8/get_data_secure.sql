13eaac4451c830935e9dcb351b9477f0e941ae78
Artyom Sovyak <46246359+Oriiie@users.noreply.github.com>
2023-01-15 04:18:30 +0200

    Create get_data_secure.sql

---
+++ Laboratory work 8/get_data_secure.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION get_data(person_name VARCHAR)
+RETURNS TABLE
+    (p_id INTEGER, p_name VARCHAR, p_age INTEGER)
+AS $$
+DECLARE
+str VARCHAR;
+BEGIN
+	str := 'SELECT p_id, person_name, age from public.persons where person_name = $1';
+	RAISE NOTICE 'Query=%',str;
+RETURN QUERY EXECUTE str USING person_name;
+END;
+$$
+LANGUAGE plpgsql;
